ccbfb882e6a26bddb8e88bbe2759e013c5381e38
josias77888 <josiasmadera06@gmail.com>
2025-06-29 17:55:37 -0500

    cambios añadidos

--- app_react/src/App.js
+++ app_react/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [mensaje, setMensaje] = useState('');
+
+  const mostrarMensaje = () => {
+    setMensaje('🎉¡FELICIDADES!🎉 te ganaste un pito 🍆 de 30 CM disfrutalo');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <p>Toca el boton para obtener un regalo</p>
+            <button className='btn-enviar' onClick={mostrarMensaje}>🎁</button>
+            <p>{mensaje}</p>
+          </div>
+      
       </header>
     </div>
   );
